1ce79814409067f366352e59e5e1688d8a12d33c
Kc Pele <fidekg123@gmail.com>
2026-02-23 18:35:29 +0100

    fix: max limit

--- ponder.config.ts
+++ ponder.config.ts
@@ -19,8 +19,9 @@ export default createConfig({
         id: 8453,
         rpc: loadBalance([
           rateLimit(http("https://base.llamarpc.com"), { requestsPerSecond: 10 }),
+          rateLimit(http("https://base-rpc.publicnode.com"), { requestsPerSecond: 10 }),
           rateLimit(http("https://mainnet.base.org"), { requestsPerSecond: 10 }),
-          rateLimit(http(process.env.BASE_RPC_URL!), { requestsPerSecond: 4 }),
+          rateLimit(http(process.env.BASE_RPC_URL!), { requestsPerSecond: 10 }),
         ]),
         ethGetLogsBlockRange: 1000,
       },
