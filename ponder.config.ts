989acb1868e6c46c137075bea021e0572c206a2b
Kc Pele <fidekg123@gmail.com>
2026-03-21 15:35:52 +0100

    fix: iccreased request

--- ponder.config.ts
+++ ponder.config.ts
@@ -19,10 +19,10 @@ export default createConfig({
       base: {
         id: 8453,
         rpc: loadBalance([
-          rateLimit(http(process.env.BASE_RPC_URL!), { requestsPerSecond: 1 }),
-          rateLimit(http(process.env.BASE_RPC_URL_2!), { requestsPerSecond: 1 }),
-          rateLimit(http(process.env.BASE_RPC_URL_3!), { requestsPerSecond: 1 }),
-          rateLimit(http(process.env.BASE_RPC_URL_4!), { requestsPerSecond: 1 }),
+          rateLimit(http(process.env.BASE_RPC_URL!), { requestsPerSecond: 25 }),
+          rateLimit(http(process.env.BASE_RPC_URL_2!), { requestsPerSecond: 25 }),
+          rateLimit(http(process.env.BASE_RPC_URL_3!), { requestsPerSecond: 25 }),
+          rateLimit(http(process.env.BASE_RPC_URL_4!), { requestsPerSecond: 25 }),
         ]),
       },
     }
