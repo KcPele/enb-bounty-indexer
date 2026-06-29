7fc78023894b1e2c07d5d4c053865925430caf06
Kc Pele <fidekg123@gmail.com>
2026-06-29 13:01:37 +0100

    fix: updated

--- ponder.config.ts
+++ ponder.config.ts
@@ -19,11 +19,12 @@ export default createConfig({
       base: {
         id: 8453,
         rpc: loadBalance([
-          //rateLimit(http(process.env.BASE_RPC_URL!), { requestsPerSecond: 7 }),
-          rateLimit(http(process.env.BASE_RPC_URL_2!), { requestsPerSecond: 7 }),
-          rateLimit(http(process.env.BASE_RPC_URL_3!), { requestsPerSecond: 7 }),
-          rateLimit(http(process.env.BASE_RPC_URL_4!), { requestsPerSecond: 7 }),
-          //rateLimit(http(process.env.BASE_RPC_URL_5!), { requestsPerSecond: 7 }),
+          rateLimit(http(process.env.BASE_RPC_URL!), { requestsPerSecond: 7 }),
+          // rateLimit(http(process.env.BASE_RPC_URL_2!), { requestsPerSecond: 7 }),
+          // rateLimit(http(process.env.BASE_RPC_URL_3!), { requestsPerSecond: 7 }),
+          // rateLimit(http(process.env.BASE_RPC_URL_4!), { requestsPerSecond: 7 }),
+          rateLimit(http(process.env.BASE_RPC_URL_5!), { requestsPerSecond: 7 }),
+          rateLimit(http(process.env.BASE_RPC_URL_6!), { requestsPerSecond: 7 }),
         ]),
       },
     }
